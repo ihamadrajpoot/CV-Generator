52b3f08fc73a388d880c6c901baedb31bf6d27d2
Muhammad Hamad <ihamadrajpoot@users.noreply.github.com>
2021-02-07 11:37:08 +0500

    Frontend : Basic Layout Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h2>My react app</h2>
+      <Navbar />
+      <Landing />
+      <Footer />
     </div>
   );
 }
